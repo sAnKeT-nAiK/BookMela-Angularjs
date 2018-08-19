1c6d99021b22c2c5cb3455391758110318a2b3b1
sAnKeT-nAiK <1100sanket@gmail.com>
2018-08-19 23:32:56 +0530

    dddd

--- app/models/models.js
+++ app/models/models.js
@@ -17,7 +17,7 @@ var db=new sqlite3.Database('../database/bookmela.db', (err) => {
 		// db.each("SELECT username,email,password from signup",function(err,row){
 		// console.log("[name= "+row.username+", email= "+row.email+", password= "+row.password+"]");
 		// });
-		db.run("CREATE TABLE IF NOT EXISTS demo(username TEXT(20), email TEXT(20), password TEXT(20))");
+		db.run("CREATE TABLE IF NOT EXISTS books (title TEXT(50), author TEXT(50), pages TEXT(10), year TEXT(10), category TEXT(30), image TEXT(200), pdf TEXT(200))");
 		
 		console.log("Database Created");
 	});
